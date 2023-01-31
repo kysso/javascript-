b3b103b1edcd6d42a30a40e6998cbef8448a5ffc
Marcos Vinicius <eumarquinhoo@gmail.com>
2023-01-31 08:38:50 -0300

    java

---
+++ ex1.js
@@ -0,0 +1,36 @@
+/*let altura =18
+let base = 8
+
+let resultado = (base*altura)/2 
+
+console.log(resultado)*/
+
+/*let n1 = 3.5
+let n2 = 7.80
+let n3 = 4.0
+
+let resultado = (n1 + n2 + n3) / 3
+console.log(`a sua media é ${resultado}`)
+*/
+
+let n1 = 25
+let n2 = 100
+let n3 = 5.50
+
+let resultado = n2*n3
+
+let x1 = 1
+let x2 = 200
+let x3 = 20.50
+
+let resultado1 = x2*x3
+
+
+
+console.log(`|-------------------------------|`)
+console.log(`|Numero de funcnionarios: ${n1} `)
+console.log(`|Sálario: R$ ${resultado} `)
+console.log(`|-------------------------------|`)
+console.log(`|Numero de funcnionarios: ${x1}      `)
+console.log(`|Sálario: R$ ${resultado1} `)
+console.log(`|-------------------------------|`)
---
+++ operadores.js
@@ -0,0 +1,8 @@
+let idade = 40
+
+if(idade >= 16 && idade < 18){
+    console.log("o voto é opcional")
+
+} else{
+    console.log("o voto é obrigatorio")
+}
